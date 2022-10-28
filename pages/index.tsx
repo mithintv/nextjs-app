87e0934b5d191c83152ac3123769b41ac706f8f7
Mithin <mithingthomas@gmail.com>
2022-10-28 14:57:46 -0400

    pre-rendering issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -20,7 +21,13 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  const [loadedMeetups, setloadedMeetups] = useState([]);
+  useEffect(() => {
+    // send an http request and fetch data
+    setloadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
 }
 
 export default HomePage;
