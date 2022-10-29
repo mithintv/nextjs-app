f692e9da4e8a03b43c91650630b9f47669bc36c8
Mithin <mithingthomas@gmail.com>
2022-10-28 21:32:38 -0400

    sending http requests to our api routes

--- models/meetup-model.ts
+++ models/meetup-model.ts
@@ -1,9 +1,12 @@
-export type MeetupType = {
-  id: string;
-  image: string;
+export type MeetupSubmissionType = {
   title: string;
+  image: string;
   address: string;
   description: string;
+};
+
+export type MeetupType = MeetupSubmissionType & {
+  id: string;
 };
 
 export type MeetupTypeList = {
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const DUMMY_MEETUPS = [
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
     address: "Some Address 10, 12345 Some City",
-    description: "This is a first meetup",
+    description: "This is a second meetup",
   },
 ];
 
