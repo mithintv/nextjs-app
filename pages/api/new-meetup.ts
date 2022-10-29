53db0d8c4e33972ed86efd6a8fa4c3a098eb077e
Mithin <mithingthomas@gmail.com>
2022-10-28 20:44:31 -0400

    working with mongodb

--- pages/api/new-meetup.ts
+++ pages/api/new-meetup.ts
@@ -1,10 +1,23 @@
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://admin:${process.env.MONGO_DB_PASS}@cluster0.80el5kh.mongodb.net/nextjs?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 }
 
